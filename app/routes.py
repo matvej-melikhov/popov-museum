f37a3e8c8731e9f950cca4ba5e161fab4e86c535
Matvej Melikhov <matvej.melikhov@yandex.ru>
2023-11-13 23:43:02 +0300

    Добавление python-файлов и html-шаблонов
    - Добавлены файлы main.py, __init__.py и app.py
    - Добавлены html-шаблоны для основных страниц: registration, apartment, laboratory, success

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template
+from app import application
+
+@application.route("/")
+def index():
+    return render_template("index.html")
+
+@application.route("/apartment-museum")
+def apartment_museum():
+    return render_template("apartment_museum.html")
+
+@application.route("/laboratory-museum")
+def laboratory_museum():
+    return render_template("laboratory_museum.html")
+
+@application.route("/registration")
+def registration():
+    return render_template("registration.html")
+
+@application.route("/success")
+def success():
+    return render_template("success.html")
+
---
+++ main.py
@@ -0,0 +1,4 @@
+from app import application
+from app.routes import *
+
+application.run(debug=True)
