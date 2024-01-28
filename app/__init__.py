a05c9c1197b90c2b81bdade66e52583618397ccd
Matvej Melikhov <matvej.melikhov@yandex.ru>
2024-01-28 11:30:00 +0300

    Форма записи, мобильное меню и перевод через Flask-Babel
    - Форма записи отправляется на /registration (POST) и редиректит на успех/ошибку
    - Исправлен дублирующийся name="name" в textarea → name="source"
    - Исправлены url_for('index') → url_for('static') в registration.html
    - Мобильное меню закрывается при клике на ссылку
    - Flask-Babel: переключатель EN/RU в шапке, сессия хранит язык
    - Навигационные ссылки обёрнуты в _() и переведены на английский

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,15 @@
-from flask import Flask
+from flask import Flask, request, session
+from flask_babel import Babel
 
 application = Flask(__name__)
 application.config['TEMPLATES_AUTO_RELOAD'] = True
+application.config['SECRET_KEY'] = 'popov-museum-dev-key'
+application.config['BABEL_DEFAULT_LOCALE'] = 'ru'
+application.config['BABEL_SUPPORTED_LOCALES'] = ['ru', 'en']
+
+def get_locale():
+    if 'lang' in session:
+        return session['lang']
+    return request.accept_languages.best_match(['ru', 'en']) or 'ru'
+
+babel = Babel(application, locale_selector=get_locale)
